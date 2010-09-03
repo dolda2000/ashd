1755d287f3cd52e739bf5b2e7392f894ee82d469
Fredrik Tolf <fredrik@dolda2000.com>
2010-09-03 06:56:24 +0200

    userplex: Actually open stdout and stderr for writing...

--- src/userplex.c
+++ src/userplex.c
@@ -80,12 +80,12 @@ static void login(struct passwd *pwd)
     /* There's whole load of other stuff one could want to do here --
      * getting Kerberos credentials, running PAM session modules, and
      * who knows what. I'll add them along as I find them useful. */
-    if(((fd = open(".ashd/output", O_APPEND)) >= 0) ||
+    if(((fd = open(".ashd/output", O_WRONLY | O_APPEND)) >= 0) ||
        ((fd = open("/dev/null", 0)) >= 0)) {
 	dup2(fd, 1);
 	close(fd);
     }
-    if(((fd = open(".ashd/error", O_APPEND)) >= 0) ||
+    if(((fd = open(".ashd/error", O_WRONLY | O_APPEND)) >= 0) ||
        ((fd = open("/dev/null", 0)) >= 0)) {
 	dup2(fd, 2);
 	close(fd);
