1d3a63cf9e3420674c97741c5d37724c2dfae0b3
Fredrik Tolf <fredrik@dolda2000.com>
2011-03-09 14:35:55 +0100

    Bumped version number.

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ htlib = Extension("ashd.htlib", ["htp.c"],
                   libraries = ["ht"])
 
 setup(name = "ashd-py",
-      version = "0.1",
+      version = "0.2",
       description = "Python module for handling ashd requests",
       author = "Fredrik Tolf",
       author_email = "fredrik@dolda2000.com",
