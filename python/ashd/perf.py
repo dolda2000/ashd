44f2a2fede8ac4655162d8c6bbd697f487d3ce3f
Fredrik Tolf <fredrik@dolda2000.com>
2011-12-22 05:24:36 +0100

    python: Pass client address and port in perf request info.

--- python/ashd/perf.py
+++ python/ashd/perf.py
@@ -16,6 +16,8 @@ if pdm:
             self.method = env.get("REQUEST_METHOD")
             self.uri = env.get("REQUEST_URI")
             self.host = env.get("HTTP_HOST")
+            self.remoteaddr = env.get("REMOTE_ADDR")
+            self.remoteport = env.get("REMOTE_PORT")
 
     class reqfinish(pdm.perf.finishevent):
         def __init__(self, start, aborted, status):
